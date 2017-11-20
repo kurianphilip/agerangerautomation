0e7864c45e3a0bd6913712f6f3464d6ee82f4a99
PhilipKurian <gittestemailtest@gmail.com>
2017-11-20 17:17:18 +1300

    "Update" script update

--- src/test/java/steps/TestUpdateAPerson.java
+++ src/test/java/steps/TestUpdateAPerson.java
@@ -12,12 +12,10 @@ public class TestUpdateAPerson {
 
     private WebDriver driver ;
 
-    String firstname;
 
-    String firstnameUpdated;
 
-    @When("^I update the details of a particular person$")
-    public void i_update_the_details_of_a_particular_person() throws Throwable {
+    @When("^I update the details of a person with name \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void i_update_the_details_of_a_person_with_name_and(String firstname, String arg2) throws Throwable {
         System.setProperty("webdriver.chrome.driver",  "C:\\Selenium\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("http://ageranger.azurewebsites.net/#/");
@@ -28,19 +26,19 @@ public class TestUpdateAPerson {
 
     }
 
-    @Then("^The details are updated$")
-    public void the_details_are_updated() throws Throwable {
+    @When("^The details are updated with \"([^\"]*)\"$")
+    public void the_details_are_updated_with(String firstnameUpdated) throws Throwable {
 
         driver.findElement(By.name("FristName")).sendKeys(firstnameUpdated);
         driver.findElement(By.tagName("form")).submit();
         driver.findElement(By.cssSelector("button[data-bb-handler^=confirm")).click();
     }
 
-    @Then("^The new data is displayed$")
-    public void the_new_data_is_displayed() throws Throwable {
+    @Then("^The new data for \"([^\"]*)\"  is displayed$")
+    public void the_new_data_for_is_displayed(String firstnameUpated) throws Throwable {
 
         Thread.sleep(500);
-        Assert.assertTrue(driver.getPageSource().contains(firstnameUpdated));
+        Assert.assertTrue(driver.getPageSource().contains(firstnameUpated));
 
 
     }
